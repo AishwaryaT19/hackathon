9d4ddb806718f27ddbc39b1b20f5ae6c16849cbd
AishwaryaT19 <aishwarya.tewari19@gmail.com>
2023-03-03 11:02:22 +0530

    new

--- pages/seating.js
+++ pages/seating.js
@@ -1,9 +1,16 @@
 import styles from "../styles/Seating.module.css";
 import React from "react";
 import Image from "next/image";
+import chair from "../public/images/chair.png";
 
 function seating() {
-  return <section className={styles.main}></section>;
+  return (
+    <section className={styles.main}>
+      <div className={styles.imgContainer}>
+        <Image src={chair} alt="main" width={100} height={100} />
+      </div>
+    </section>
+  );
 }
 
 export default seating;
