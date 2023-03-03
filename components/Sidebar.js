28a2c546cba52d48b5aa011a9f5da852cfa037b2
Anant Shukla <anant.official2201@gmail.com>
2023-03-03 11:01:38 +0530

    commit anant1

---
+++ components/Sidebar.js
@@ -0,0 +1,14 @@
+import styles from "../styles/Sidebar.module.css"
+
+const Sidebar = () => {
+  return (
+    <>
+    <div className={styles.aside}>
+        
+    </div>
+    </>
+    
+  )
+}
+
+export default Sidebar
--- pages/seating.js
+++ pages/seating.js
@@ -1,9 +1,9 @@
 import styles from "../styles/Seating.module.css";
-import React from "react";
-import Image from "next/image";
 
 function seating() {
-  return <section className={styles.main}></section>;
+  return <section className={styles.main}>
+
+  </section>;
 }
 
 export default seating;
